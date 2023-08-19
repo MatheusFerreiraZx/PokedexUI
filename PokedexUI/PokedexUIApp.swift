c36d33a9eb4efbd8d3b492429ff8b9e9d7c30b47
Matheus Ferreira <mathferreiranasc12@gmail.com>
2023-08-19 19:57:43 -0300

    Initial Commit

---
+++ PokedexUI/PokedexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexUIApp.swift
+//  PokedexUI
+//
+//  Created by Matheus Ferreira on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
