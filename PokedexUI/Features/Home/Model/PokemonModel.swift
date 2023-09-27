160fa4f207419f184bad92e133d4941e3cf48f85
Jonata Klabunde <jonata_k@yahoo.com.br>
2023-09-27 15:21:08 -0300

    create type

---
+++ PokedexUI/Features/Home/Model/PokemonModel.swift
@@ -0,0 +1,23 @@
+//
+//  Pokemon.swift
+//  PokedexUI
+//
+//  Created by Matheus Ferreira on 19/08/23.
+//
+
+import SwiftUI
+
+struct PokemonModel: Codable, Identifiable {
+    
+    enum PokemonType: String, Codable {
+        case fire, poison, water, electric, psychic, normal, ground, flying, fairy, none
+    }
+    
+    let id: Int
+    let name: String
+    let imageUrl: String
+    private let type: String
+    var pokemonType: PokemonType {
+        return PokemonType(rawValue: type) ?? .none
+    }
+}
